1e196561f3797ad7570a40a55a11e99348cc234b
Serge V. Kior <svkior@gmail.com>
2015-07-14 00:51:30 +0300

    Day End Changes

--- artnet_test/cmd/assets/app/views/footer.jsx
+++ artnet_test/cmd/assets/app/views/footer.jsx
@@ -5,7 +5,7 @@ var ArtGateFooter = React.createClass({
     render: function(){
         return (
             <footer className="site-footer">
-                Разработано &copy; 2014-2015 ООО "Театральные Технологические Системы" <br/>
+                Р Р°Р·СЂР°Р±РѕС‚Р°РЅРѕ &copy; 2014-2015 РћРћРћ "РўРµР°С‚СЂР°Р»СЊРЅС‹Рµ РўРµС…РЅРѕР»РѕРіРёС‡РµСЃРєРёРµ РЎРёСЃС‚РµРјС‹" <br/>
                 Tel: +7 495 730-83-45, +7 495 730-83-46<br/>
                 Web: <a href="http://www.ttsy.ru" target="_blank">http://www.ttsy.ru</a> <br/>
                 E-Mail: <a href="mailto:info@ttsy.ru">info@ttsy.ru</a>
